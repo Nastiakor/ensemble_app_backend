2ff06ec5fdafad7f77276a4d266b70c269963d65
Anastasia Korotkova <a.korotkova@yahoo.fr>
2026-03-20 11:54:10 +0100

    refactor: remove default app controller and service

--- src/activities/activities.controller.spec.ts
+++ src/activities/activities.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ActivitiesController } from './activities.controller';
+import { ActivitiesService } from './activities.service';
 
 describe('ActivitiesController', () => {
   let controller: ActivitiesController;
 
+  const mockActivitiesService = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ActivitiesController],
+      providers: [
+        {
+          provide: ActivitiesService,
+          useValue: mockActivitiesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ActivitiesController>(ActivitiesController);
